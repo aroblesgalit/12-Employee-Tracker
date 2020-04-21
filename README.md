2133864146722b63be766bab207455ae4f120260
Alvin Galit <aroblesgalit@gmail.com>
2020-04-21 17:16:26 -0500

    Created schema

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Unit 12 MySQL Homework: Employee Tracker
+# 12 Employee Tracker
+> Command line interface for tracking employees within a company.
 
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. Often these interfaces are known as **C**ontent **M**anagement **S**ystems. In this homework assignment, your challenge is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
 
---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT FOREIGN KEY REFERENCES role(id),
+    manager_id INT FOREIGN KEY REFERENCES role(id) NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT FOREIGN KEY REFERENCES department(id) NOT NULL
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+

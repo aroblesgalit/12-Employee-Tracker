bed876f2e16adc06f553f440667e6e204a032bed
Alvin Galit <aroblesgalit@gmail.com>
2020-04-22 19:21:12 -0500

    Added more comments in main js

--- main.js
+++ main.js
@@ -15,23 +15,37 @@ main();
 
 async function mainMenu() {
     try {
+        // Prompt for mainMenu()
         const { action } = await questions.mainMenu();
         if (action === "Exit") {
+            // End connection
             queryHelper.connection.end();
         } else if (action === "View all employees") {
+            // Use queryHelper to view all employees
             const data = await queryHelper.viewAllEmployees();
+            // Log data to a table
             console.table(data);
+            // Run mainMenu()
             mainMenu();
         } else if (action === "View all employees by department") {
+            // Prompt user for a department
             const { department } = await questions.departmentPrompt();
+            // Then pass the chosen department into query viewAllByDepartment
             const data = await queryHelper.viewAllByDepartment(department);
+            // Log data to a table
             console.table(data);
+            // Run mainMenu()
             mainMenu();
         } else if (action === "View all employees by Manager") {
+            // Get manager list using query getAllManager()
             const managerList = await queryHelper.getAllManager();
+            // Prompt user for a manager
             const { manager } = await questions.managerPrompt(managerList);
+            // Split manager's name into an array of first and last name
             const managerName = await manager.split(" ");
+            // Pass first and last name to query viewAllByManager
             const data = await queryHelper.viewAllByManager(managerName[0], managerName[1]);
+            // Log data to a table
             console.table(data);
             mainMenu();
         } else if (action === "Add an employee") {
